83665bc0aa74a1c726d0418886631acd249a9e1c
Aleksandr Panchul <apanchul@hotmail.com>
2017-10-01 21:52:36 -0400

    regex snippet

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # sb_python
+
 Sandbox for Python programming, templates and reusable snippets
+
+This repository could be a useful starting point for writing scripts, or demoing how to write scripts.
+It could be used on its own, or deployed on a virtual machine within the workspace [https://github.com/panchul/workspace](https://github.com/panchul/workspace)
+
+The ```/doc``` folder of the ```workspace``` repo has the documentation. 
+
+I will gradually migrate the notes I have to this repository.
+
+
+## UNDER CONSTRUCTION
+
+
---
+++ snippets/mypass.py
@@ -0,0 +1,24 @@
+#
+# run is as
+# $ python mypass.py
+#
+
+import re
+
+# Passwords will contain at least (1) upper case letter
+# Passwords will contain at least (1) lower case letter
+# Passwords will contain at least (1) number or special character
+# Passwords will contain at least (8) characters in length
+# Password maximum length is 64
+def test_password(password):
+   # return password # 
+   return re.match(r'(?=^.{8,64}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$', password)
+
+
+print("now lets try it")
+
+print test_password("govno")
+print test_password("g2vNoasc34&56")
+print test_password("govno")
+
+
