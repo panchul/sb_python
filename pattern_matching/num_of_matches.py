52782a2195ef15bf37107f49f4cda4cc2107d6e3
Aleksandr Panchul <apanchul@hotmail.com>
2018-11-13 14:00:32 -0500

    adding regression demo

---
+++ pattern_matching/num_of_matches.py
@@ -0,0 +1,12 @@
+#
+# $ echo sometext more sometext | python -i num_of_matches.py 
+# Number of matches : 2
+# >>> 
+#
+
+import re
+Test_String = raw_input()
+Regex_Pattern = r'sometext'
+match = re.findall(Regex_Pattern, Test_String)
+print "Number of matches :", len(match)
+
