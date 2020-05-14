d72f1cd25b10aee875d7c9be508077a01ecefad3
Aleksandr Panchul <apanchul@hotmail.com>
2020-05-13 22:05:04 -0700

    adding get opt sample

---
+++ snippets/cli_params.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# % python cli_params.py govno
+# Number of arguments: 2 arguments.
+# Argument List: ['cli_params.py', 'govno']
+#
+
+import sys
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+
