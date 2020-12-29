b3df9a75611b7e89c340f27054b0912282101f37
Aleksandr Panchul <panchul@users.noreply.github.com>
2020-12-29 15:56:24 -0800

    adding downloading facebook video files

---
+++ networking_web_download/dodownloading.py
@@ -0,0 +1,27 @@
+
+# urllib, urllib2, urllib3
+# https://docs.python.org/3/howto/urllib2.html
+
+#
+#	$ python -m pip install urllib3
+#
+
+# https://www.facebook.com/panchul/videos/10156791602064936
+import re
+import requests
+import urllib.request
+
+#link = input("url: ")
+link = "https://www.facebook.com/panchul/videos/10156791602064936"
+html = requests.get(link)
+
+try:
+    url = re.search('hd_src:"(.+?)"',html.text)[1]
+    print("hd")
+except:
+    url = re.search('sd_src:"(.+?)"',html.text)[1]
+    print("sd")
+
+print(f"downloading {url}...")
+urllib.request.urlretrieve(url,"test.mp4")
+print("Done")
